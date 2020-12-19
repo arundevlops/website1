f667dd148946737a9d47be68bdb9220ae152cee6
arundevlops <btech.arung@gmail.com>
2020-12-19 15:39:30 +0530

    'a'

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
-#This Terraform Code Deploys Basic VPC Infra.
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
-    region = "us-east-1"
+    region = "${var.region}"
 }
---
+++ state-backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "backentstatefile"
+    key    = "terraform/myterraform.tfstate"
+    region = "us-east-1"
+  }
+}
